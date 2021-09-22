921d6d8af249129f4499ee4d869c31dff2177fde
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-09-22 11:42:53 +0300

    create artCard component #5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ArtCard from './Components/ArtCard';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [results, setResults] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <ArtCard results={results} loading={loading}/>
     </div>
   );
 }
