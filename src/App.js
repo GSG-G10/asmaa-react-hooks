405e2be9775c01cc7cbde77252a6c475dd60d9a3
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-09-22 12:30:07 +0300

    display artworks #7

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import { useState } from 'react';
-import './App.css';
-import ArtCard from './Components/ArtCard';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import { Container } from "react-bootstrap";
+import ArtCard from "./Components/ArtCard";
+import { getArtworks } from "./utils/getArtworks";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [results, setResults] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getArtworks()
+      .then((searchResults) => {
+        if (searchResults && searchResults.data) {
+          setResults(searchResults.data);
+        }
+      })
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <ArtCard results={results} loading={loading}/>
+      <header></header>
+      <Container fluid>
+        {error ? (
+          <p>Unable to retrieve results.</p>
+        ) : (
+          <ArtCard results={results} loading={loading} />
+        )}
+      </Container>
     </div>
   );
 }
