79d7c2d88677c6100d7550e68656072a178c8a31
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-09-22 10:16:36 +0300

    create get artworks function, and image url function #3

---
+++ src/utils/getArtworks.js
@@ -0,0 +1,11 @@
+export const getArtworks = () => {
+  const url =
+    "https://api.artic.edu/api/v1/artworks/search?q=nature&limit=25&fields=id,title,image_id,thumbnail";
+  return fetch(url)
+    .then((res) => res.json())
+    .catch((err) => {
+      throw new Error(`Error in fetching art ${err}`);
+    });
+};
+
+export const imageUrl = (id, width = 843) => `https://www.artic.edu/iiif/2/${id}/full/${width},/0/default.jpg`;
